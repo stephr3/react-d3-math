6a34007f95f0882d1434bc2ed53e61cbc9abcd7c
Gurung <sgurung@ALPHA.local>
2016-10-24 15:26:40 -0700

    display line

--- src/components/data-line.jsx
+++ src/components/data-line.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
 export default (props) => {
-  return <g><line x1='0' x2='10' y1='2' y2='11' />
+  return <g><line x1='50' x2='70' y1='35' y2='55' stroke='black'/>
   </g>
 }
